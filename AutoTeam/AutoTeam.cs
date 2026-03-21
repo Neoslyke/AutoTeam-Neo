eecf1f1763260548ddb43720c91542dd560ad2a2
Neoslyke <al_reminiscence@yahoo.com>
2026-03-21 21:13:44 +0800

    Standardized author and version information

--- AutoTeam/AutoTeam.cs
+++ AutoTeam/AutoTeam.cs
@@ -9,11 +9,11 @@ namespace AutoTeam;
 [ApiVersion(2, 1)]
 public class AutoTeam : LazyPlugin
 {
-    public override string Author => "Modified by Neoslyke (original by 十七 / 肝帝熙恩)";
-    public override Version Version => new Version(2, 4, 13);
+    public override string Name => "AutoTeam";
+    public override string Author => "Neoslyke, 十七，肝帝熙恩)";
+    public override Version Version => new Version(2, 1, 0);
     public override string Description => "Automatically assigns players to teams based on their group";
-    public override string Name => System.Reflection.Assembly.GetExecutingAssembly().GetName().Name!;
-
+    
     public AutoTeam(Main game) : base(game) { }
 
     public override void Initialize()
